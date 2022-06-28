fc40f4c587179aa46d26cb9a60455b5d482b60ef
Michael Dear <michaelcdear@gmail.com>
2022-06-28 10:21:22 -0600

    commit tree 14c3b9d5b53ad475687464da5f620f58132130ee

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = scuberArray => scuberArray.slice(0, 2)
+
+const returnLastTwoDrivers = scuberArray => scuberArray.slice(2, 4)
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(x) {
+    return function fareMultiplier (fare) {
+        return fare * x
+    }
+}
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+const fareQuintupler = createFareMultiplier(5)
+
+function selectDifferentDrivers (scuberArray, drivers) {
+    return drivers(scuberArray)
+}
